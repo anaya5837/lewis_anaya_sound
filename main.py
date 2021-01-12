b87b5b626f570211bf0c3a2647710c91bdf78dbe
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-01-12 10:28:53 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("Sound Level: " + input.sound_level()) 
-    if input.sound_level() > 30:
+    if input.sound_level() < 30:
         light.set_all(light.rgb(100, 100, 100))
     else:
         light.clear
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("Sound Level: " + input.soundLevel())
-    if (input.soundLevel() > 30) {
+    if (input.soundLevel() < 30) {
         light.setAll(light.rgb(100, 100, 100))
     } else {
         light.clear
